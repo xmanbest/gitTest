3eac0dbcf2b7e0e8464f248e448add9b729ea0a7
李建華 <lijianhuax@icloud.com>
2016-03-23 19:58:56 +0800

    第一次尝试github提交代码

---
+++ FirstGitTest/FirstGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstGitTest
+//
+//  Created by 李建華 on 16/3/23.
+//  Copyright © 2016年 lijianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstGitTest/FirstGitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstGitTest
+//
+//  Created by 李建華 on 16/3/23.
+//  Copyright © 2016年 lijianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
